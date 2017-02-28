4b5ab19dd8f3459723a7f446ffd2dfcb1705c0da
victor.blanco <victor.blanco@lstkin101783.softtek.com>
2017-02-28 14:09:11 -0600

    Ejemplos de Response
    Metodos dummy de response

--- lab-5/lab-5-sentence-ribbon/src/main/java/com/example/LockyController.java
+++ lab-5/lab-5-sentence-ribbon/src/main/java/com/example/LockyController.java
@@ -1,26 +1,90 @@
 package com.example;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class LockyController {
-	
-	private static final Logger LOGGER = Logger.getLogger(LockyController.class);
 
-	@Value("${info}") String info;
-	
+	@Value("${info}")
+	String info;
+
+	public class TestPerson {
+		private String nombre;
+		private int edad;
+		private String domicilio;
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public int getEdad() {
+			return edad;
+		}
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		public String getDomicilio() {
+			return domicilio;
+		}
+
+		public void setDomicilio(String domicilio) {
+			this.domicilio = domicilio;
+		}
+
+	}
+
 	@Autowired
 	LoadBalancerClient client;
+
+	@RequestMapping("/person")
+	public @ResponseBody TestPerson showPersonExample() {
+		TestPerson person = new TestPerson();
+		person.setNombre("dummy");
+		Calendar calendar = Calendar.getInstance();
+		person.setEdad(calendar.get(Calendar.YEAR));
+		person.setDomicilio("N/A");
+		return person;
+	}
+
+	@RequestMapping("/persons")
+	public @ResponseBody List<TestPerson> showPersonsExample(@RequestParam("names") String names,
+			@RequestParam("ages") int[] ages, @RequestParam("addresses") String addresses) {
+		List<TestPerson> list = null;
+		if (names != null && !names.isEmpty()) {
+			String[] namesSplit = names.split(",");
+			String[] addressesSplit = addresses.split(",");
+			list = new ArrayList<LockyController.TestPerson>();
+			int i = 0;
+			for (String namesIterator : namesSplit) {
+				TestPerson person = new TestPerson();
+				person.setNombre(namesIterator);
+				person.setEdad(ages[i]);
+				person.setDomicilio(addressesSplit[i]);
+				list.add(person);
+				i++;
+			}
+		}
+		return list;
+	}
 
 	@RequestMapping("/sentence")
 	public @ResponseBody String showLuckyWord() {
@@ -32,19 +96,16 @@ public class LockyController {
 		builder.append(getWord("LAB-4-NOUN")).append(" ");
 		return builder.toString();
 	}
-	
+
 	@RequestMapping("/info")
-	public @ResponseBody String showServerInfo(){
+	public @ResponseBody String showServerInfo() {
 		return info;
 	}
 
 	private String getWord(String service) {
-		LOGGER.info("Load Balancer CLient >>>>>>>>"+client);
 		ServiceInstance serviceInstance = client.choose(service);
-		LOGGER.info("Service Instance>>>>>>"+serviceInstance);
-		if (serviceInstance != null ) {
+		if (serviceInstance != null) {
 			URI uri = serviceInstance.getUri();
-			LOGGER.info("URI>>>>>>>>>>>>>>"+uri.toString());
 			if (uri != null) {
 				return (new RestTemplate()).getForObject(uri, String.class);
 			}
